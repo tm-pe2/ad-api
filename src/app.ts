fb81ba37eb3c909521a7439f4aa9bd6053714c96
panda jacobs <jokke.jacobs@gmail.com>
2022-02-18 22:01:54 +0100

    docker + mailService basis

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import {MailService} from './services/mailService'
+
+if (process.env.NODE_ENV == null || process.env.NODE_ENV === 'develepmont') {
+  dotenv.config();
+}
+
+(async () => {
+  console.log("run");
+  new MailService().testEmail()
+})();
