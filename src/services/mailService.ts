b759ecf66817ef3edc66e29585a7467736c566cc
LAPTOP-3BHD5BUF\panda <jokke.jacobs@gmail.com>
2022-02-24 12:09:50 +0100

    use built in func for testing

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -1,37 +1,52 @@
-import nodemailer from 'nodemailer'
+import { logging } from 'googleapis/build/src/apis/logging';
+import {TestAccount, Transporter, createTestAccount, createTransport,} from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 import * as mailConfig from '../../mailServiceConfig.json'
 
 export class MailService {
-  private transport: nodemailer.Transporter<SMTPTransport.SentMessageInfo>;
-  private hostEmail: string;
+  private account: TestAccount | undefined;
+  private transport: Transporter<SMTPTransport.SentMessageInfo> | undefined;
+  //private hostEmail: string;
   //using mailtrap.io -> mail development enviroment
-  constructor() {
-    this.hostEmail = mailConfig.host + "@" + mailConfig.domain;
-    const transportConfig :SMTPTransport.Options = {
-      host: process.env.MAILSERVER_HOST,
-      port: mailConfig.port,
-      secure: mailConfig.SSL,
-      logger: true,
-      requireTLS: mailConfig.TSL,
-      auth: {
-        type: "LOGIN",
-        password: "beep",
-        user: this.hostEmail,
-      }
-    };
-    if (process.env.MAILTRAP_U && process.env.MAILTRAP_P) {
-      transportConfig.auth = { 
-        user: process.env.MAILTRAP_U,
-        pass: process.env.MAILTRAP_P
-      };
-      //throw new Error("please define MAILTRAP_U and MAILTRAP_P in your .env file")
-    }
-    //TODO fix secure connection
-    //set the port to use SSL or TLS -> put in certificate -> generate certificate
-    this.transport = nodemailer.createTransport(transportConfig);
+  constructor() { 
+    this.acc();
+    this.transport = this.createTrans();
+    
     this.verify();
   };
+
+  private async acc() {
+    const acc = await createTestAccount();
+    if(!acc)
+    {
+      throw new Error("couldn't create test account");
+    }
+  }
+
+  private async createTrans() {
+    return
+    })
+
+
+    //env check
+    /*if(!process.env.MAILSERVICE_USER || !process.env.MAILSERVICE_PASS)
+      throw new Error("please define MAILSERVICE_USER and/or MAILSERVICE_PASS");
+    */
+
+    //this.hostEmail = process.env.MAILSERVICE_USER + "@" + mailConfig.domain;
+    //using https://ethereal.email includes authentication
+    /*this.transport = nodemailer.createTransport({
+      host: mailConfig.host,
+      port: mailConfig.port,
+      secure: mailConfig.secure,
+      requireTLS: mailConfig.secure,
+      auth: {
+          user: this.hostEmail,
+          pass: process.env.MAILSERVICE_PASS
+      },
+      logger: true
+  });*/
+  }
 
   private async verify(): Promise<void> {
     if (!(await this.transport?.verify()))
@@ -44,7 +59,7 @@ export class MailService {
   
 
     this.transport.sendMail({
-      from: this.hostEmail,
+      //from: this.hostEmail,
       to: userEmail,
       text: "test",
       subject: "test"
@@ -53,3 +68,4 @@ export class MailService {
 
 
 };
+
