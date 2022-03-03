573a094bfb1b8fc8a1ede087df6558c38c46ea9d
LAPTOP-3BHD5BUF\panda <jokke.jacobs@gmail.com>
2022-03-03 12:02:55 +0100

    start example send invoice

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -2,27 +2,29 @@ import { Logger } from '../utils/logger';
 import { Transporter, createTransport, } from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 import * as mailConfig from '../../mailServiceConfig.json'
+import { Customer } from '../models/customers';
 
 export class MailService {
   private transport: Transporter<SMTPTransport.SentMessageInfo>;
   private hostEmail: string;
-
+  private endMail = `Best regards, ${mailConfig.company}`
+  
   constructor() {
     this.checkEnv()
     this.hostEmail = process.env.MAILSERVICE_USER + "@" + mailConfig.domain;
     this.transport = this.createTrans();
     this.verify()
   };
-
+  
   private checkEnv() {
     if (!process.env.MAILSERVICE_USER)
-      throw new Error("Please define MAILSERVICE_USER in your env file");
-
-    if (!process.env.MAILSERVICE_PASS)
-      throw new Error("Please define MAILSERVICE_PASS in your env file");
-    }
+    throw new Error("Please define MAILSERVICE_USER in your env file");
     
-    private createTrans() {
+    if (!process.env.MAILSERVICE_PASS)
+    throw new Error("Please define MAILSERVICE_PASS in your env file");
+  }
+  
+  private createTrans() {
     //using https://ethereal.email includes authentication, testing
     return createTransport({
       host: mailConfig.host,
@@ -42,17 +44,22 @@ export class MailService {
       throw new Error("transport in mailService isn't valid");
   }
 
-  public testEmail() {
+  public sendInvoice() {
     //supposed to be gotten from the data base
-    const userEmail = "maci.rohan48@ethereal.email"
+    const customer : Customer = {id: 1,
+    firstName: "maci", lastName: "rohan48", email: "maci.rohan48@ethereal.email"}
+    const title = `Dear ${customer.firstName} ${customer.lastName}`;
+    const body = ["this is a test", "a longer test message is what this is. maybe you can give me some juice."];
 
     
     this.transport.sendMail({
       from: this.hostEmail,
-      to: userEmail,
+      to: customer.email,
       text: "test",
       subject: "test"
     }).catch((e) => {Logger.error(e);})
   }
+
+
 };
 
