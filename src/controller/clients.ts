9ae3ac6ab6ec2b1f98e82656419d2e0e22403a68
EndriFucia <99872056+EndriFucia@users.noreply.github.com>
2022-03-07 13:32:57 +0100

    Database class added and queries for the routes available.

--- src/controller/clients.ts
+++ src/controller/clients.ts
@@ -1,6 +1,6 @@
 /** source/controllers/clients.ts */
 import { Request, Response, NextFunction } from 'express';
-var mysql = require('mysql');
+import { Database } from '../calsses/database';
 
 interface Address {
     street: String;
@@ -17,22 +17,13 @@ interface Client {
     address: Address;
 }
 
-var conn = mysql.createConnection({
-    host: "localhost",
-    user: "Webuser",
-    password: "Lab2021",
-    database: "webshop"
-});
-
-conn.connect(function (err: Error) {
-    if (err) {
-        console.log("error occured while connecting");
-    }
-});
+let db = new Database();
+let conn = db.connect();
 
 // get all clients
 const getClients = async (req: Request, res: Response, next: NextFunction) => {
-    conn.query("Select * FROM users", function (err: Error, clients: string) 
+    let query: string = "Select * FROM users";
+    conn.query(query, function (err: Error, clients: string) 
     {
         if (err) throw err;
         return res.status(200).json({
@@ -46,6 +37,14 @@ const getClients = async (req: Request, res: Response, next: NextFunction) => {
 const getClient = async (req: Request, res: Response, next: NextFunction) => {
     // get the client id from the req
     let id: string = req.params.id;
+    let query: string = "Select * FROM users Where UID = " + id;
+    conn.query(query, function (err: Error, client: string) 
+    {
+        if (err) throw err;
+        return res.status(200).json({
+        client
+        });
+    });
 };
 
 // update a client
@@ -53,19 +52,48 @@ const updateClient = async (req: Request, res: Response, next: NextFunction) => 
     // get the client id from the req.params
     let id: string = req.params.id;
     // get the data from req.body
-    //let name: string = req.body.name ?? null;
+    let { name, lastname, imagePath, type, username, password } = req.body;
+    let query: string = `UPDATE users SET name="${name}", lastname="${lastname}",
+                            imagePath="${imagePath}", type="${type}",
+                            username="${username}", password="${password}"
+                            Where UID = ` + id;
+    
+    conn.query(query, function (err: Error, client: string) 
+    {
+        if (err) throw err;
+        return res.status(200).json({
+            client
+        });
+    });
 };
 
 // delete a client
 const deleteClient = async (req: Request, res: Response, next: NextFunction) => {
     // get the client id from req.params
     let id: string = req.params.id;
+    let query: string = "DELETE FROM users Where UID = " + id;
+    conn.query(query, function (err: Error, client: string) 
+    {
+        if (err) throw err;
+        return res.status(200).json({
+        client
+        });
+    });
 };
 
 // add a client
 const addClient = async (req: Request, res: Response, next: NextFunction) => {
     // get the data from req.body
-    //let name: string = req.body.name ?? null;
+    let { name, lastname, imagePath, type, username, password } = req.body;
+
+    let query: string = `INSERT INTO users (name, lastname, imagePath, type, username, password) VALUES ("${name}","${lastname}","${imagePath}","${type}","${username}","${password}")`;
+    conn.query(query, function (err: Error, client: string) 
+    {
+        if (err) throw err;
+        return res.status(200).json({
+        client
+        });
+    });
 };
 
 export default { getClients, getClient, updateClient, deleteClient, addClient };
