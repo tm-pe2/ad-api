6872d009e4f2ffa92a5598f1678ab1d3ee993e47
panda jacobs <jokke.jacobs@gmail.com>
2022-04-20 15:56:14 +0200

    also check if there is a mail that's rejected

--- src/__tests__/mail_service.test.ts
+++ src/__tests__/mail_service.test.ts
@@ -5,17 +5,19 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 describe("sents invoice mails", () => {
-    it("should respond defined", async ()=> {
+    it("should respond 250 ...", async () => {
         const ms = new MailService();
         const res = await ms.sendInvoice();
-        expect(res.response).toEqual(expect.stringContaining(`250 Accepted`));
+        expect(res.response).toEqual(expect.stringContaining(`250`));
+        expect(res.rejected.length).toEqual(0);
     })
 })
 
-describe("sents workorder", ()=> {
-    it("should respond defined",async () => {
+describe("sents workorder", () => {
+    it("should respond 250 ...", async () => {
         const ms = new MailService();
         const res = await ms.sendWorkOrder();
-        expect(res.response).toEqual(expect.stringContaining(`250 Accepted`));
+        expect(res.response).toEqual(expect.stringContaining(`250`));
+        expect(res.rejected.length).toEqual(0);
     })
-} )
+})
