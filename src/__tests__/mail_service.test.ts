2948d956529ab2658b929432d8b67d45487eb895
panda jacobs <jokke.jacobs@gmail.com>
2022-05-05 17:22:43 +0200

    update mail service test

--- src/__tests__/mail_service.test.ts
+++ src/__tests__/mail_service.test.ts
@@ -1,5 +1,5 @@
 import { SentMessageInfo } from "nodemailer";
-import { MailService } from "../services/mailService";
+import { MailService } from "../services/mail-service";
 import dotenv from 'dotenv';
 
 dotenv.config()
@@ -7,7 +7,20 @@ dotenv.config()
 describe("sents invoice mails", () => {
     it("should respond 250 ...", async () => {
         const ms = new MailService();
-        const res = await ms.sendInvoice();
+        const res = await ms.sendInvoice({
+            InvoiceID: 1,
+            CustomerID: 1,
+            DueDate: new Date(),
+            SupplierID: 1,
+            Date: new Date(),
+            Status: 0,
+            GasAmount: 0,
+            ElectricityType: 0,
+            Price: 0,
+            Tax: 0,
+            StartDate: new Date(),
+            EndDate: new Date()
+        });
         expect(res.response).toEqual(expect.stringContaining(`250`));
         expect(res.rejected.length).toEqual(0);
     })
