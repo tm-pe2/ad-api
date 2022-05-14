4d39a36183dd17678e552a75b4b60dec4f889b6e
DrunkenToast <peter5leconte@gmail.com>
2022-05-14 15:28:29 +0200

    Change self route order

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -5,9 +5,9 @@ import { UserRole } from '../models/userrole';
 
 const router = express.Router();
 
+router.get('/self', userController.getUserSelf);
 router.get('/', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER]), userController.getAllUsers);
 router.get('/:id', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER]), userController.getUserById);
-router.get('/self', userController.getUserSelf);
 router.post('/', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER, UserRole.CUSTOMER]), userController.addUser);
 
 export = router;
