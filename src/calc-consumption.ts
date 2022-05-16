05191f74403fe8dd871d8077ff4ef8179d0a8967
Paulien <r0810768@student.thomasmore.be>
2022-05-16 21:40:54 +0200

    file added

--- src/calc-consumption.ts
+++ src/calc-consumption.ts
@@ -1,2 +1,18 @@
-// if a new customer is made
-// meter_id
+// every month this should run
+
+//query to get all meters
+
+//for all meters that are smart meters:
+    // 
+
+fetch("http://10.97.0.100:3000/meter/${meter_id}/update", {
+    method: "PATCH",
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+})
+.then(
+    (response) => {
+        // response.consumption ofzo -> querie voor nieuwe consumption toe te voegen uit voeren en klaar
+    }
+)
