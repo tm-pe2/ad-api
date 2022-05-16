f2153c3773a7e3289907a9fcc1878e318ccc9617
EndriFucia <99872056+EndriFucia@users.noreply.github.com>
2022-05-17 00:40:17 +0200

    Validation code clean up.

--- src/validations/contract-validation.ts
+++ src/validations/contract-validation.ts
@@ -1,5 +1,4 @@
 import {Contract} from '../classes/contracts';
-import * as customerService from '../services/customer-service';
 
 const errorList = {
     customer:`Customer with this account does not exist!`,
@@ -7,37 +6,12 @@ const errorList = {
     activeContract:`Contract already exists!`
 }
 
-// const checkActiveContract = async (contract: Contract) => {
-//     const result =  await contractService.getContractsByCustomerAndAddressID(contract.customer_id,contract.service_type,contract.address_id,contract.end_date);
-//     if(result.length > 0)
-//     {
-//         return false;
-//     }
-//     return true;
-// }
-
-// const checkCustomerExists = async (id:number): Promise<boolean> => {
-//     if(!!await customerService.getCustomerById(id))
-//     {
-//         return false;
-//     }
-//     return true;
-// }
-
-const checkDate = (startDate: Date) => {
-    let date = new Date();
-    if(startDate < date)
-    {
-        return false;
-    }
-    return true;
-}
-
 export const checkContractData =async (contract: Contract) => {
     let errorVar: string = '';
+    let date = new Date();
     try 
     {
-        if(!checkDate(contract.start_date))
+        if(contract.start_date < date)
         {
             errorVar = errorList.startDate;
             throw new Error(errorList.startDate);
