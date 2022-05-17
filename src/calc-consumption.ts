893fcdc577065713aa02538acebdbaaece91dbc9
Paulien <r0810768@student.thomasmore.be>
2022-05-17 20:10:13 +0200

    trying something

--- src/calc-consumption.ts
+++ src/calc-consumption.ts
@@ -1,3 +1,6 @@
+import {Consumtion} from "./classes/consumption"
+import * as consumptionService from "./services/consumption-service";
+import fetch from "node-fetch";
 // every month this should run
 
 //query to get all meters
@@ -5,7 +8,7 @@
 //for all meters that are smart meters:
     // 
 
-fetch("http://10.97.0.100:3000/meter/${meter_id}/update", {
+const response = fetch("http://10.97.0.100:3000/meter/1/update", {
     method: "PATCH",
     headers: {
       'content-type': 'application/json;charset=UTF-8',
@@ -14,5 +17,9 @@ fetch("http://10.97.0.100:3000/meter/${meter_id}/update", {
 .then(
     (response) => {
         // response.consumption ofzo -> querie voor nieuwe consumption toe te voegen uit voeren en klaar
+        console.log(response);
     }
+)
+.catch(
+    (error) => console.log('error', error)
 )
