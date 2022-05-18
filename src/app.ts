eb3b362ad4901cb38917ef9b24fe923a48331d1c
panda jacobs <jokke.jacobs@gmail.com>
2022-05-18 15:20:11 +0200

    remove testing mail in app

--- src/app.ts
+++ src/app.ts
@@ -22,23 +22,5 @@ try {
 
     const PORT: any = process.env.PORT || 6060;
     httpServer.listen(PORT, () => Logger.info(`The server is running on port ${PORT}`));
-    try {
-        const ms = new MailService();
-       console.log(await ms.sendInvoice({
-    InvoiceID: 1, CustomerID: 1, 
-    DueDate: new Date(), 
-    SupplierID: 1,
-    Date: new Date(),
-    Status: 0,
-    GasAmount: 0,
-    ElectricityType: 0,
-    Price: 0,
-    Tax: 0,
-    StartDate: new Date(),
-    EndDate: new Date()
-}));
-        ms.sendWorkOrder();
-    } catch (e) {
-        console.error(e);
-    }
-})();
+    
+});
