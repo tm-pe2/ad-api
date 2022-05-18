71e4a151ff57baeb86620287d28ab6d319703ab7
EndriFucia <99872056+EndriFucia@users.noreply.github.com>
2022-05-18 22:32:41 +0200

    Removed comments

--- src/routes/contract-routes.ts
+++ src/routes/contract-routes.ts
@@ -7,8 +7,7 @@ const router = express.Router();
 
 router.get('/', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER]), contractController.getAllContracts);
 router.get('/:id', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER, UserRole.CUSTOMER]),contractController.getContractById);
-//router.post('/', auth.authenticate([UserRole.ADMIN, UserRole.CUSTOMER]),contractController.addContract);
-router.post('/', contractController.addContract);
+router.post('/', auth.authenticate([UserRole.ADMIN, UserRole.CUSTOMER]),contractController.addContract);
 router.put('/', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER, UserRole.CUSTOMER]),contractController.updateContract);
 router.delete('/:id', auth.authenticate([UserRole.ADMIN]), contractController.deleteContractById);
 
--- src/routes/meter-routes.ts
+++ src/routes/meter-routes.ts
@@ -7,8 +7,7 @@ const router = express.Router();
 
 router.get('/', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER]), meterController.getAllMeters);
 router.get('/:id', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER, UserRole.CUSTOMER]),meterController.getMeterById);
-//router.post('/', auth.authenticate([UserRole.ADMIN, UserRole.CUSTOMER]),meterController.addMeter);
-router.post('/', meterController.addMeter);
+router.post('/', auth.authenticate([UserRole.ADMIN, UserRole.CUSTOMER]),meterController.addMeter);
 router.put('/', auth.authenticate([UserRole.ADMIN, UserRole.MANAGER, UserRole.CUSTOMER]),meterController.updateMeter);
 router.delete('/:id', auth.authenticate([UserRole.ADMIN]), meterController.deleteMeterById);
 
