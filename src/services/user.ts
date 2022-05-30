fbe38d9df3701f47c5fb7afee127b20b0d753808
DrunkenToast <peter5leconte@gmail.com>
2022-05-30 21:15:18 +0200

    Query for user auth info

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,59 +1,25 @@
 import {execute} from "../utils/database-connector";
-import {User} from "../models/user";
-import {userQueries} from "../queries/users-queries";
+import {User, UserAuthInfo} from "../models/user";
+import {userQueries} from "../queries/users";
 
-export const getAllUsers = async () => {
-    return await execute<User[]>(userQueries.getAllUsers, []);
-};
+export async function getUserAuthInfoById(id: number): Promise<UserAuthInfo | null> {
+    const res = await execute(userQueries.getUserAuthInfoById, [id]);
+    if (res.rowCount === 0) return null;
+    return {
+        id: res.rows[0].id,
+        email: res.rows[0].email,
+        password: res.rows[0].password,
+        roles: res.rows[0].roles
+    }
+}
 
-export const getUserById = async (id: User['id']) => {
-    const users = await execute<User[]>(userQueries.getUserById, [id]);
-    return users[0];
-};
-
-export const getUserByEmail = async (email: User['email']) => {
-    const result = await execute<User[]>(userQueries.getUserByEmail, [email])
-    return result[0];
-};
-
-
-export const getUserByNationalNumber = async (nationalNumber: User['national_registry_number']) => {
-    const users = await execute<User[]>(userQueries.getUserByNationalNumber, [nationalNumber]);
-
-    return users[0];
-};
-
-export const addUser = async (user: User) => {
-    // TODO : update
-    const newUser = await execute<User[]>(userQueries.AddUser, [
-        user.first_name,
-        user.last_name,
-        user.birth_date,
-        user.email,
-        user.phone_number,
-        user.national_registry_number
-    ]);
-
-    return newUser[0].id;
-};
-
-export const updateUser = async (user: User) => {
-    // TODO: update
-    const rowCount = await execute<number>(userQueries.UpdateUser, [
-        user.id,
-        user.first_name,
-        user.last_name,
-        user.birth_date,
-        user.email,
-        user.phone_number,
-        user.id
-    ]);
-
-    return rowCount > 0;
-};
-
-export const deleteUser = async (id: User['id']) => {
-    const rowCount = await execute<number>(userQueries.DeleteUserById, [id]);
-
-    return rowCount > 0;
-};
+export async function getUserAuthInfoByEmail(email: string): Promise<UserAuthInfo | null> {
+    const res = await execute(userQueries.getUserAuthInfoByEmail, [email]);
+    if (res.rowCount === 0) return null;
+    return {
+        id: res.rows[0].id,
+        email: res.rows[0].email,
+        password: res.rows[0].password,
+        roles: res.rows[0].roles
+    }
+}
