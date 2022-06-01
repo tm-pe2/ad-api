5874ef7060d55ce53fd8576c89c0a06f1f1118b2
panda jacobs <jokke.jacobs@gmail.com>
2022-06-01 23:24:36 +0200

    token to user id function (verifyToken)

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,10 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
 import jwt, { TokenExpiredError } from "jsonwebtoken";
+import { ParsedQs } from "qs";
+import { AccessTokenData } from "../classes/accesstokens";
 import { getAccessToken } from "../middleware/auth";
+import { User } from "../models/user";
 import * as userService from "../services/user";
 
 export class UserController {
@@ -10,19 +14,10 @@ export class UserController {
             if (process.env.JWTSECRET == undefined) {
                 throw new Error('JWTSECRET undefined');
             }
-            getAccessToken(req)
-                .then((token) => {
-                    jwt.verify(token, process.env.JWTSECRET!, (err: any, decoded: any) => {
-                        if (err) {
-                            if (err instanceof TokenExpiredError) {
-                                return res.status(401).send('Unauthorized: Access token expired.')
-                            }
-                            return res.status(401).send('Unauthorized.')
-                        }
-        
-                        if (decoded.id != undefined) {
-                            userService.getUserById(decoded.id)
-                                .then((user) => {
+            this.verifyToken(req).then(token => {
+                if (token.id != undefined) {
+                            userService.getUserById(token.id)
+                                .then((user: User) => {
                                     res.status(200).json(
                                         user
                                     );
@@ -36,10 +31,29 @@ export class UserController {
                         else {
                             res.status(403).send('Forbidden: Resource access denied, insufficient rights.')
                         }
+            })
+        
+                        
+                    
+                
+            })
+        }
+     
+ 
+        //TODO move to auth?
+    static async verifyToken(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>) : Promise<AccessTokenData>{
+        return new Promise((resolve, reject) => {
+            getAccessToken(req)
+                .then((token) => {
+                    jwt.verify(token, process.env.JWTSECRET!, (err: any, decoded: any) => {
+                        if (err) {
+                            if (err instanceof TokenExpiredError) {
+                                reject('Unauthorized: Access token expired.')
+                            }
+                            reject('Unauthorized.')
+                        }
+                        resolve(decoded as AccessTokenData);
                     })
-                })
-                .catch((err) => {
-                    res.status(401).send(err);
                 })
         })
     }
