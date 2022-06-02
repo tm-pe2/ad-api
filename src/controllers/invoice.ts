de06bc59a421fb91c01eb67bf0f7e5bd4e4190c2
LAPTOP-3BHD5BUF\panda <jokke.jacobs@gmail.com>
2022-06-02 14:35:23 +0200

    /self route

--- src/controllers/invoice.ts
+++ src/controllers/invoice.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { verifyToken } from "../middleware/auth";
 import { Invoice } from "../models/invoice";
 import { invoiceQueries } from "../queries/invoice-queries";
 import { execute } from "../utils/database-connector";
@@ -16,7 +17,14 @@ export class InvoiceController {
         })
 
         .get('/self', (req, res, next) => {
-            //get id from login
+            verifyToken(req).then(async (token) =>{
+                const invoices = await (await execute(invoiceQueries.getInvoiceByUserId, [token.id])).rows as Invoice[]
+                if(invoices)
+                    return res.status(200).send(invoices)
+                else
+                    res.status(502).json({error: "there was an error when trying to get invoices from user: " + token.id})
+            }
+            )
         })
     }
 
