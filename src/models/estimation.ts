ebe783b44aee0c155f51b31cb24db712c70e2f81
DrunkenToast <peter5leconte@gmail.com>
2022-06-09 23:01:35 +0200

    Feat: GET/POST /estimation

--- src/models/estimation.ts
+++ src/models/estimation.ts
@@ -1,10 +1,19 @@
 // TODO: verify interfaces and types
 
+import { Address } from "cluster";
+
+export interface Estimation {
+    id: number;
+    past_consumption: number;
+    estimated_consumption: number;
+    address: Address
+}
+
 export interface EstimationRegistration {
+    past_consumption: number;
+    address_id: number;
     family_size: number,
-    address_id: number,
     building_type: BuildingType,
-    past_consumption: number,
     service_type: ServiceType,
     meters: Meter[],
     equipment: EquipmentType[],
@@ -17,19 +26,19 @@ interface Meter {
     value: number,
 }
 
-enum BuildingType {
+export enum BuildingType {
     APARTMENT = 0,
     CLOSED = 1,
     SEMI_DETACHED = 2,
     OPEN = 3,
 }
 
-enum ServiceType {
+export enum ServiceType {
     ELECTRICITY = 0,
     GAS = 1,
 }
 
-enum EquipmentType {
+export enum EquipmentType {
     OVEN_STOVE = 1,
     DISHWATER = 2,
     WASHING_MACHINE = 3,
