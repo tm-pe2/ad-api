19ea801f4877460c07b14d47ea3eee325a0a7c55
Dries1234 <driesolbrechts@gmail.com>
2022-06-10 13:27:41 +0200

    query changes

--- src/queries/employee.ts
+++ src/queries/employee.ts
@@ -1,28 +1,27 @@
 // get employee by id
 // add fields for corresponding user to the result
-
-const getEmployeeById = `
+const selectEmployeeQuery = `
     SELECT
-        u.id,
-        e.hire_date,
-        e.salary,
-        u.first_name,
-        u.last_name,
-        u.birth_date,
-        u.email,
-        u.phone_number,
-        u.national_registry_number,
-        json_agg(
-            json_build_object(
-                'id', a.id,
-                'street', a.street,
-                'house_number', a.house_number,
-                'city_name', c.city_name,
-                'postal_code', c.postal_code,
-                'country', a.country
-            )
-        ) as addresses,
-        array_agg(r.id) as roles
+    u.id,
+    e.hire_date,
+    e.salary,
+    u.first_name,
+    u.last_name,
+    u.birth_date,
+    u.email,
+    u.phone_number,
+    u.national_registry_number,
+    json_agg(
+        json_build_object(
+            'id', a.id,
+            'street', a.street,
+            'house_number', a.house_number,
+            'city_name', c.city_name,
+            'postal_code', c.postal_code,
+            'country', a.country
+        )
+    ) as addresses,
+    array_agg(r.id) as roles
     FROM employees as e
     JOIN users as u ON e.user_id = u.id
     JOIN users_addresses as ua ON u.id = ua.user_id
@@ -30,16 +29,14 @@ const getEmployeeById = `
     JOIN users_roles as ur ON u.id = ur.user_id
     JOIN roles as r ON ur.role_id = r.id
     JOIN cities_postalcodes as c ON a.city_id = c.id
-    WHERE e.user_id = $1
-    GROUP BY u.id, u.first_name, u.last_name, u.birth_date, u.email, u.phone_number, u.national_registry_number, e.hire_date, e.salary
-    `;
+`
+
+
+
 const insertEmployee = `
     INSERT INTO employees (user_id, hire_date, salary) VALUES ($1, $2, $3)
 `
 
-const getAllEmployees = `
-    SELECT user_id, hire_date, salary FROM employees
-`
 
 //modify employee
 const modifyEmployee = `
@@ -57,8 +54,16 @@ const modifyAddress = `
 `
 export const employeeQueries = {
     insertEmployee: insertEmployee,
-    getAllEmployees: getAllEmployees,
-    getEmployeeById: getEmployeeById,
+    
+    getAllEmployees: selectEmployeeQuery + `
+        GROUP BY u.id, e.hire_date, e.salary
+    `,
+
+    getEmployeeById: selectEmployeeQuery + `
+        WHERE e.user_id = $1
+        GROUP BY u.id, u.first_name, u.last_name, u.birth_date, u.email, u.phone_number, u.national_registry_number, e.hire_date, e.salary;
+    `,
+
     modifyEmployee: modifyEmployee,
     modifyUser: modifyUser,
     modifyUserRoles: modifyUserRoles,
