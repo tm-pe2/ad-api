788770443f5d7491e699d428276c9408dc3b7840
Dries1234 <driesolbrechts@gmail.com>
2022-06-12 22:46:53 +0200

    fix error that slipped in when rewriting queries

--- src/queries/customer.ts
+++ src/queries/customer.ts
@@ -19,12 +19,12 @@ const selectCustomerQuery = `
                     'city_name',ci.city_name, 
                     'postal_code',ci.postal_code, 
                     'country',a.country)
-                ) as a,
+                ) as addresses,
             array_agg(r.id) as roles
         FROM ${TABLES.CUSTOMERS} as c
         LEFT JOIN ${TABLES.USERS} as u ON c.user_id = u.id
         LEFT JOIN ${TABLES.USERS_ADDRESSES} as ua ON u.id = ua.user_id
-        LEFT JOIN ${TABLES.ADDRESSES} as ON ua.address_id = a.id
+        LEFT JOIN ${TABLES.ADDRESSES} as a ON ua.address_id = a.id
         LEFT JOIN ${TABLES.USERS_ROLES} as ur ON u.id = ur.user_id
         LEFT JOIN ${TABLES.ROLES} as r ON ur.role_id = r.id
         LEFT JOIN ${TABLES.CITIES} as ci ON a.city_id = ci.id
