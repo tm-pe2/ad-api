4c8cb3b9c6876f25e52a376a17c02ad6feb2fc7c
DrunkenToast <peter5leconte@gmail.com>
2022-06-15 17:23:58 +0200

    in fix

--- src/classes/validate.ts
+++ src/classes/validate.ts
@@ -96,7 +96,7 @@ export class ValidateInterface {
             throw new Error("No company name provided");
         Validate.checkVatNumber(supplier.vat_number);
         Validate.checkAddresses([supplier.address]);
-        if (!(supplier.service_type in ['gas', 'electricity']))
+        if (!(['gas', 'electricity'].includes(supplier.service_type)))
             throw new Error("Invalid service type");
     }
         
