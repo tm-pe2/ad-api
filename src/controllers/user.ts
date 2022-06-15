fa91da0c269c9d5aca2d165e442b17cbb4bfe1a9
panda jacobs <jokke.jacobs@gmail.com>
2022-06-15 22:33:44 +0200

    user status patch

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,26 +5,27 @@ import { ParsedQs } from "qs";
 import { AccessTokenData } from "../classes/accesstokens";
 import { authSelf, getAccessToken } from "../middleware/auth";
 import { User } from "../models/user";
+import { userQueries } from "../queries/users";
 import * as userService from "../services/user";
-import { begin } from "../utils/database-connector";
+import { begin, execute } from "../utils/database-connector";
 
 export class UserController {
     static router(): Router {
         return Router({ caseSensitive: false })
             .get('/self', authSelf(), async (req, res, next) => {
                 const client = await begin();
-                userService.getUserById(client,req.body.tokenData.id)
+                userService.getUserById(client, req.body.tokenData.id)
                     .then((user: User | null) => {
                         if (user === null) {
                             res.status(404).send("User not found");
-                        } else {                           
+                        } else {
                             res.status(200).json(
                                 user
                             );
                         }
                     })
                     .catch((err) => {
-                        if(err instanceof Error){
+                        if (err instanceof Error) {
                             console.log(err.stack)
                         }
                         res.status(500).json({
@@ -33,6 +34,21 @@ export class UserController {
                     });
                 client.release();
             })
+            .patch('/status/:id', async (req, res, next) => {
+                try {
+                  const client = await begin();
+                  const status = req.body.status
+                  if (status instanceof Boolean) {
+                      const id = await execute(client, userQueries.changeStatus, [req.params.id, status])
+                      res.status(200).send({message: "set status of user: " + id + " to " + status})
+                  } else{
+                      res.status(406).json({message: "status is not a boolean"})
+                  }
+                } catch (error) {
+                  res.status(500).json({message: "something went wrong updating the user"})
+                }
+                  
+              })
     }
 }
 
